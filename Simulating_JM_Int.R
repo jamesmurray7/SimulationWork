91631f765aa7722b5069a9ee5b2de96fe6477f7e
parqueted <j.murray7@ncl.ac.uk>
2020-11-19 13:17:59 +0000

    Functionised simple case, separate analysis to follow...

--- Simulating_JM_Int.R
+++ Simulating_JM_Int.R
@@ -51,7 +51,7 @@ summary(lmer(Y ~ x1 + (1|id), data = long_dat))
 lambda <- 1 # BL Hazard
 
 uu <- runif(m)
-survtime <- -log(uu)/(lambda * exp(b1*trt))
+survtime <- -log(uu)/(lambda * exp(b1*trt + U_int))
 length(which(survtime > 5))/length(survtime) # ~ 50% experience event with lambda = 1
 
 ratec <- 0.05
@@ -89,5 +89,54 @@ fit <- joineR::joint(joineR_joint_dat,
               model = "int",
               sepassoc = F, max.it = 50,
               verbose = T)
-summary(fit) # 
+summary(fit) 
 
+
+# Functionise -------------------------------------------------------------
+# Just random intercept and one binary covariate, again.
+
+joint_sim <- function(m = 500, n_i = 6, 
+                      b0 = 40, b1 = -10, 
+                      sigma.i = 1.5, sigma.e = 2.5,
+                      lambda = 0.5){
+  # Set out variables
+  N <-  m * n_i
+  id <- 1:m
+  time <- 0:(n_i-1)
+  tau <- max(time) 
+  U_int <- rnorm(m, 0, sigma.i) # Random effects
+  x <- rbinom(m, 1, 0.5) # Treatment assignment per id
+  
+  # Longitudinal part //
+  
+  xl <- rep(x, each = n_i)
+  U <- rep(U_int, each = n_i)
+  epsilon <- rnorm(N, 0, sigma.e)
+  Y <- b0 + U + b1 * xl + epsilon
+  
+  long_dat <- data.frame(id = rep(id, each = n_i),
+                         time = rep(time, m),
+                         xl, Y)
+  
+  # Survival part     //
+  
+  # Survival times
+  u <- runif(m)
+  survtime <- -log(u)/(lambda * exp(b1 * x))
+  
+  # Censoring and truncation
+  rateC <- 0.05
+  censor <- rexp(m, rateC)
+  status <- ifelse(censor < survtime, 0, 1)
+  survtime <- pmin(survtime, censor, tau)
+  
+  surv_dat <- data.frame(id, x, survtime, status)
+  
+  # Extra output
+  pc_events <- length(which(survtime < tau))/m * 100
+  
+  return(list(long_dat, surv_dat, pc_events))
+  
+}
+
+# Separate investigation
