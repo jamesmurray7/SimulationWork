d6d2f82891d44f6373c5e8267d46a19de5ecc0a4
parqueted <j.murray7@ncl.ac.uk>
2020-11-19 10:59:37 +0000

    Re-doing longitudinal part so survival is easier

--- Simulating_JM_Int.R
+++ Simulating_JM_Int.R
@@ -2,6 +2,7 @@
 #' Simulating joint data to fit a joint model to.
 #' This only uses a random intercept for the L.A.
 #' and fairly trivial covariate(s), until it works!
+#' Taking exponential baseline hazard
 #' ###
 
 # Prerequisites ------------------------------------------------------------
@@ -30,10 +31,25 @@ n_i <- 6 # Number of measurements (=length(t))
 N <- m*n_i
 
 # Data
-x <- rep(rbinom(m, 1, 0.5), each = n_i)
-U <- rep(rnorm(m, 0, sigma.i), each = n_i) # Random effects
+trt <- rbinom(m, 1, 0.5) 
+U_int <- rnorm(m, 0, sigma.i)
+
+id <- rep(1:m, each = n_i)
+x1 <- rep(trt, each = n_i) # 'extending down' for all times
+U <- rep(U_int, each = n_i) # Random effects
 tt <- rep(t, m)
 epsilon <- rnorm(N, 0, sigma.e)
 
-Y <- b0 + U + b1 * x + epsilon
-long_dat <- data.frame(x, tt, Y)
+# Longitudinal part ----
+
+Y <- b0 + U + b1 * x1 + epsilon
+long_dat <- data.frame(id, x1, tt, Y)
+
+summary(lmer(Y ~ x1 + (1|id), data = long_dat))
+
+# Survival part ----
+lambda <- 1 # BL Hazard
+
+uu <- runif(m)
+survtime <- -log(uu)/(lambda * exp(b1*trt))
+length(which(survtime > 6))/length(survtime)
